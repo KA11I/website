11a2ddb5d387c04eff3830fe6822a6a3e032b2af
KA11I <75325289+KA11I@users.noreply.github.com>
2022-06-10 02:09:07 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,22 @@ function typewriter() {
   }
 }
 
-let i = 4;
+let changeImageBool = true;
+
 async function changeImage() {
-  i = i - 1
-  document.querySelector("#imgClickAndChange").src = "crepe/Crepe" + i + ".png"
+  if (changeImageBool) {
+    changeImageBool = false;
+    const images = [
+      "crepe/Crepe3.png",
+      "crepe/Crepe2.png",
+      "crepe/Crepe1.png",
+      "crepe/Crepe0.png",
+    ];
+    for (let img of images) {
+      document.querySelector("#imgClickAndChange").src = img;
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
 }
+
 document.querySelector("#imgClickAndChange").onclick = changeImage;
