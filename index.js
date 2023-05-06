294d0505f165507c06c950c159bb2036cad915f9
KA11I <75325289+KA11I@users.noreply.github.com>
2023-05-06 16:28:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ var txt = "HEY I'M KARL";
 var speed = 100;
 
 function typewriter() {
-    if (i < txt.length) {
-        document.getElementById("title").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typewriter, speed);
-    }
+  if (i < txt.length) {
+    document.getElementById("title").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typewriter, speed);
+  }
 }
 
 let c = 4;
 async function changeImage() {
-    document.querySelector("#imgClickAndChange").src = `crepe/Crepe${c-1>=0?c-1:0}.png`, c--
+  document.querySelector("#imgClickAndChange").src = `crepe/Crepe${c-1>=0?c-1:0}.png`, c--
 }
 document.querySelector("#imgClickAndChange").onclick = changeImage;
 
-  var nightpic = document.getElementById("nightpic");
-  
-  nightpic.addEventListener("click", function() {
-    nightpic.src = "karl night burn.gif";
-    setTimeout(function() {
-      nightpic.src = "karl night.jpg";
-    }, 3000);
-  });
+var nightpic = document.getElementById("nightpic");
+
+nightpic.addEventListener("click", function() {
+  nightpic.src = "karl night burn.gif";
+  setTimeout(function() {
+    nightpic.src = "karl night.jpg";
+  }, 3000);
+});
 
 typewriter();
